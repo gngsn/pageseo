e22fe6445f7b61fdc1985db40769a72e4a69742b
gngsn <rudtjs4540@gmail.com>
2020-04-24 15:42:50 +0900

    update login project

--- login_project/routes/user.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const userController = require('../controllers/userController');
-
-router.post('/signin', userController.signin);
-router.post('/signup', userController.signup);
-
-module.exports = router;
