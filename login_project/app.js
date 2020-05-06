569b8ed5ca33f9947c046f0e70a2313d97660f94
gngsn <rudtjs4540@gmail.com>
2020-05-06 21:05:34 +0900

    remove unnecessary dir & files

--- login_project/app.js
+++ login_project/app.js
@@ -7,9 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
