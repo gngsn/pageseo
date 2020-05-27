85853c115d71a11b2a65323477a569106067cb1a
gngsn <rudtjs4540@gmail.com>
2020-05-27 15:13:52 +0900

    Update api project

--- api_project/routes/user.js
+++ api_project/routes/user.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/userController');
-
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'User ~' });
+const multer = require('multer');
+const upload = multer({
+    dest: 'uploads/'
 });
-router.post('/signin', userController.signin);
-router.post('/signup', userController.signup);
 
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'User ~'
+    });
+});
+
+router.post('/signin', userController.signin);
+router.post('/signup', upload.single('profile'), userController.signup);
+// router.post('/profile', upload.single('image'), userController.signup);
 module.exports = router;
