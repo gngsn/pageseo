a865358fc34368f0a8ba3838a92c398d4f8661bf
gngsn <rudtjs4540@gmail.com>
2020-06-03 20:25:19 +0900

    Update S3-project

---
+++ s3-project/controllers/user.js
@@ -0,0 +1,34 @@
+const util = require('../modules/util');
+var multer = require('multer');
+
+module.exports = {
+    uploadImage: async (req, res) => {
+        const image = req.file.path;
+        if (image === undefined) {
+            return res.status(400).send(util.fail(400, "이미지가 존재하지 않습니다."));
+        }
+        res.status(200).send(util.success(200, "요청 성공 〰️ ", image));
+    },
+    uploadImages : async (req, res) => {
+        const image = req.files;
+        const path = image.map(img => img.path);
+        if (image === undefined) {
+            return res.status(400).send(util.fail(400, "이미지가 존재하지 않습니다."));
+        }
+        res.status(200).send(util.success(200, "요청 성공 〰️ ", path));
+    },
+    uploadMultiImages: async (req, res) => {
+        const image = req.files;
+        const backImage = image.background[0];
+        const profiles = image.profiles;
+        const profilePath = profiles.map(img => img.path);
+        if (image === undefined) {
+            return res.status(400).send(util.fail(400, "이미지가 존재하지 않습니다."));
+        }
+        const dto = {
+            backImage : backImage.path,
+            profiles : profilePath
+        }
+        res.status(200).send(util.success(200, "요청 성공 〰️ ", dto));
+    }
+}
