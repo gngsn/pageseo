9f67ae70a65f169c45a7004f4f59738f7493f22f
gngsn <rudtjs4540@gmail.com>
2020-06-16 19:55:37 +0900

    Delete socket project 2

--- memo-project2/README.md
+++
@@ -1,7 +0,0 @@
-#  ✨ simple memo project  ✨
-
-
-
-#### 🖍 tutorial :  [Express+React, 간단한 프로젝트 만들기](https://gngsn.tistory.com/17)
-
-#### 🖍 test-site :  [Click-here](http://13.209.144.115:3000/memo)
--- memo-project2/server/routes/chat.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const ChatController = require('../controllers/chat');
-
-router.get('/', ChatController.readAll);
-router.post('/', ChatController.write);
-
-module.exports = router;
