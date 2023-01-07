3ddc89cf9bc76ffd0ded273bf69241210314a760
gngsn <rudtjs4540@gmail.com>
2023-01-07 17:10:52 +0900

    update: upgrade node modules

--- sequelize-project/app.js
+++ sequelize-project/app.js
@@ -8,9 +8,8 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
