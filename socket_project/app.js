dff09467f204959427874ffc0b9bfe5ce8af9d23
gngsn <rudtjs4540@gmail.com>
2023-01-07 17:07:14 +0900

    update: upgrade node modules

--- socket_project/app.js
+++ socket_project/app.js
@@ -24,7 +24,7 @@ app.io.on('connection',(socket) => {
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
